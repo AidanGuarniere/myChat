896094945c0018c8fe0cb988a0be3916162030ed
aidan <aidanguarniere@gmail.com>
2023-02-13 13:02:58 -0800

    added title key to conversations state

--- components/Chatbox.jsx
+++ components/Chatbox.jsx
@@ -27,7 +27,7 @@ function ChatGPTChatbox() {
         const completion = response.data.completion;
         setConversations([
           ...conversations,
-          { prompt: userText, response: completion },
+          { title: userText, prompt: userText, response: completion },
         ]);
         setLoading(false);
       } catch (error) {
@@ -72,6 +72,7 @@ function ChatGPTChatbox() {
       <div className="flex">
         <ConversationHistory
           conversations={conversations}
+          userText={userText}
           setUserText={setUserText}
           setSelectedConversation={setSelectedConversation}
         />
@@ -95,64 +96,58 @@ function ChatGPTChatbox() {
               {loading ? "Loading..." : "Submit"}
             </button>
           </form>
-          {conversations.length > 0 ? (
-            <div className="conversation">
-              {selectedConversation !== null &&
-              conversations[selectedConversation] ? (
+          <div className="conversation">
+            {conversations[selectedConversation] ? (
+              <div className="bg-white rounded p-4 mt-4 h-auto">
+                <div className="conversation-item" key={selectedConversation}>
+                  <div className="bg-white rounded p-4 mt-4 h-auto">
+                    {selectedConversation === 0 ? (
+                      <div>
+                        {" "}
+                        <p className="text-black-700 font-bold break-words px-2 max-h-100%">
+                          {conversations[0].prompt}
+                        </p>
+                        <p className="text-gray-700 break-words px-2 max-h-100%">
+                          {conversations[selectedConversation]
+                            ? conversations[0].response
+                            : "Loading..."}
+                        </p>
+                      </div>
+                    ) : (
+                      <div>
+                        {" "}
+                        {selectedConversation && (
+                          <p className="text-black-700 font-bold break-words px-2 max-h-100%">
+                            {conversations[selectedConversation].prompt}
+                          </p>
+                        )}
+                        {selectedConversation && (
+                          <p className="text-gray-700 break-words px-2 max-h-100%">
+                            {conversations[selectedConversation]
+                              ? conversations[selectedConversation].response
+                              : "Loading..."}
+                          </p>
+                        )}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="conversation-history">
+                <h2 className="text-xl font-medium mb-2">
+                  Start a Conversation:
+                </h2>
                 <div className="bg-white rounded p-4 mt-4 h-auto">
                   <div className="conversation-item" key={selectedConversation}>
                     <div className="bg-white rounded p-4 mt-4 h-auto">
-                      {selectedConversation === 0 ? (
-                        <div>
-                          {" "}
-                          <p className="text-black-700 font-bold break-words px-2 max-h-100%">
-                            {conversations[0].prompt}
-                          </p>
-                          <p className="text-gray-700 break-words px-2 max-h-100%">
-                            {conversations[selectedConversation]
-                              ? conversations[0].response
-                              : "Loading..."}
-                          </p>
-                        </div>
-                      ) : (
-                        <div>
-                          {" "}
-                          {selectedConversation && (
-                            <p className="text-black-700 font-bold break-words px-2 max-h-100%">
-                              {conversations[selectedConversation].prompt}
-                            </p>
-                          )}
-                          {selectedConversation && (
-                            <p className="text-gray-700 break-words px-2 max-h-100%">
-                              {conversations[selectedConversation]
-                                ? conversations[selectedConversation].response
-                                : "Loading..."}
-                            </p>
-                          )}
-                        </div>
-                      )}
+                      <p className="text-gray-700 break-words px-2 max-h-100%"></p>
                     </div>
                   </div>
                 </div>
-              ) : (
-                <div className="conversation-history">
-                  <h2 className="text-xl font-medium mb-2">
-                    Start a Conversation:
-                  </h2>
-                  <div className="bg-white rounded p-4 mt-4 h-auto">
-                    <div
-                      className="conversation-item"
-                      key={selectedConversation}
-                    >
-                      <div className="bg-white rounded p-4 mt-4 h-auto">
-                        <p className="text-gray-700 break-words px-2 max-h-100%"></p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              )}
-            </div>
-          ) : null}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- components/ConversationHistory.jsx
+++ components/ConversationHistory.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
+import React from "react";
 
-function ConversationHistory({conversations, setUserText, setSelectedConversation}) {
+function ConversationHistory({
+  conversations,
+  userText,
+  setUserText,
+  setSelectedConversation,
+}) {
   return (
     <div className="w-1/4 h-auto max-h-100% bg-gray-200 rounded-lg shadow-lg p-10 mr-2 overflow-hidden">
-    <div className="conversation-history-sidebar">
-      <h2 className="text-xl font-medium mb-2">Conversation History:</h2>
-      {conversations.map((conversation, index) => (
-        <div
-          className="conversation-item"
-          key={index}
-          onClick={() => {
-            setUserText("");
-            setSelectedConversation(index);
-          }}
-        >
-          <h3 className="text-lg font-medium mb-2">
-            {conversation.prompt}
-          </h3>
-        </div>
-      ))}
+      <div className="conversation-history-sidebar">
+        <h2 className="text-xl font-medium mb-2">Conversation History:</h2>
+        {conversations.map((conversation, index) => (
+          <div
+            className="conversation-item"
+            key={index}
+            onClick={() => {
+              if (userText.length) {
+                setUserText("");
+              }
+              setSelectedConversation(index);
+            }}
+          >
+            <h3 className="text-lg font-medium mb-2">{conversation.title}</h3>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>  )
+  );
 }
 
-export default ConversationHistory
+export default ConversationHistory;
