9292d8098f0b68248ed3d3264338ffd31a41a9d3
aidan <aidanguarniere@gmail.com>
2023-05-19 12:34:46 -0700

    general UI improvements

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="md:pl-[289px] relative bottom-[5.75rem] md:bottom-14 left-0 w-full md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient">
+    <div className="md:pl-[260px] relative bottom-[5.75rem] md:bottom-14 left-0 w-full md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient">
       <div className="h-10 w-4/5 flex justify-center items-start sm:items-center mx-auto">
         <span className="text-[.625rem] sm:text-[.75rem] text-gray-600 dark:text-gray-300 text-center">
           myChat is not affiliated with OpenAI. myChat is an open source project
